ebc84ac003508fcd74d13dfb306695dcb2bc3eff
Ricardo Pineda Lazcano <urielink81@gmail.com>
2021-02-17 15:05:31 -0600

    limpiar plugin

--- gallery/gallery.js
+++
@@ -1,9 +0,0 @@
-// Verificar que funciona.
-console.log('Gallery script');
-
-// Registrar bloque de muestra.
-wp.blocks.registerBlockStyle( 'core/gallery', {
-    name: 'bootstrap-gallery',
-    label: 'Bootstrap gallery',
-} );
-
--- quote/quote.js
+++
@@ -1,9 +0,0 @@
-// Verificar que funciona.
-console.log('Quote script');
-
-// Registrar bloque de muestra.
-wp.blocks.registerBlockStyle( 'core/quote', {
-    name: 'fancy-quote',
-    label: 'Fancy Quote',
-} );
-
