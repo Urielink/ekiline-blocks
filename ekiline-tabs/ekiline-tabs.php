6ea33cab1f4c146b51a811f36aaee29d7de67726
Ricardo Pineda Lazcano <urielink81@gmail.com>
2021-07-29 00:14:14 -0500

    Correccion en el registro para empaquetar ZIP

--- ekiline-tabs/ekiline-tabs.php
+++ ekiline-tabs/ekiline-tabs.php
@@ -21,6 +21,45 @@
  * @see https://developer.wordpress.org/block-editor/tutorials/block-tutorial/writing-your-first-block-type/
  */
 function ekiline_blocks_ekiline_tabs_block_init() {
-	register_block_type( __DIR__ );
+
+	$dir = __DIR__;
+
+	$script_asset_path = "$dir/build/index.asset.php";
+	if ( ! file_exists( $script_asset_path ) ) {
+		throw new Error(
+			'You need to run `npm start` or `npm run build` for the "ekiline-blocks/ekiline-tabs" block first.'
+		);
+	}
+	$index_js     = 'build/index.js';
+	$script_asset = require( $script_asset_path );
+	wp_register_script(
+		'ekiline-blocks-ekiline-tabs-block-editor',
+		plugins_url( $index_js, __FILE__ ),
+		$script_asset['dependencies'],
+		$script_asset['version']
+	);
+	wp_set_script_translations( 'ekiline-blocks-ekiline-tabs-block-editor', 'ekiline-tabs' );
+
+	$style_css = 'build/style-index.css';
+	wp_register_style(
+		'ekiline-blocks-ekiline-tabs-block',
+		plugins_url( $style_css, __FILE__ ),
+		array(),
+		filemtime( "$dir/$style_css" )
+	);
+
+	// register_block_type( __DIR__ );
+	// Esta clase, se agrega por registrar con el package.json (wp-block-ekiline-blocks-ekiline-tabs).
+	// Solo me funciona al desarrollar.
+
+	register_block_type(
+		'ekiline-blocks/ekiline-tabs',
+		array(
+			'apiVersion'      => 2,
+			'editor_script'   => 'ekiline-blocks-ekiline-tabs-block-editor',
+			'editor_style'    => 'ekiline-blocks-ekiline-tabs-block-editor',
+			'style'           => 'ekiline-blocks-ekiline-tabs-block',
+		)
+	);
 }
 add_action( 'init', 'ekiline_blocks_ekiline_tabs_block_init' );
