3824f064a73d40e1bf5d7ef091457a36fbaef97e
Ricardo Pineda Lazcano <urielink81@gmail.com>
2022-01-12 12:54:35 -0600

    actualizacion licencia

--- ekiline-blocks.php
+++ ekiline-blocks.php
@@ -33,7 +33,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Ekiline Blocks. If not, see {URI to Plugin License}.
+along with Ekiline Blocks. If not, see http://www.gnu.org/licenses/gpl-2.0.txt.
 */
 
 /**
