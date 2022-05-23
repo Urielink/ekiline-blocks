7111d42a1f7b2d5605f541141960d3206352d119
Ricardo Pineda Lazcano <urielink81@gmail.com>
2022-05-22 20:51:06 -0500

    construido.

--- ekiline-modal/build/index.asset.php
+++ ekiline-modal/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'e1069e130cf415c17956ab6062e66889');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '6bebc26d3c705cd7cf01eabdda24a452');
