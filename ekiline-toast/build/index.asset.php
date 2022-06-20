18ebeaac680d3e1185a8de4d57a81bd50a81222e
Ricardo Pineda Lazcano <urielink81@gmail.com>
2022-06-20 18:09:12 -0500

    toast v1, falta trigger con scroll.

--- ekiline-toast/build/index.asset.php
+++ ekiline-toast/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a305432138454810cc8b');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'aa9095809d387e6b9cb4');
